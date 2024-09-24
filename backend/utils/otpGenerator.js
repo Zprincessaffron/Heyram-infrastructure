1c27c697f95aa10920d70ad59f4aff54fe133bcd
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-09-24 12:30:24 +0530

    otp

--- backend/utils/otpGenerator.js
+++ backend/utils/otpGenerator.js
@@ -3,3 +3,4 @@ import otpGenerator from 'otp-generator';
 export const generateOtp = () => {
     return otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, digits: true, lowerCaseAlphabets: false });
 };
+ 
